81416c96a2fe26fba0892c1274505640ead66940
Daniel Muri <danmur26@gmail.com>
2023-09-22 16:21:52 +0200

    before main layout

--- src/store/index.js
+++ src/store/index.js
@@ -1,58 +1,77 @@
 // store.js
 import { create } from "zustand";
 
+const initialState = () => {
+  const storedCart = localStorage.getItem("cart");
+  return storedCart ? JSON.parse(storedCart) : [];
+};
+
 const useStore = create((set) => ({
-  cart: [],
+  cart: initialState(),
+
+  setCart: (cart) => {
+    localStorage.setItem("cart", JSON.stringify(cart)); // Save to localStorage
+    set({ cart });
+  },
   addProductToCart: (product) =>
     set((state) => {
       const existingProductIndex = state.cart.findIndex(
-        (p) => p.slug === product.slug,
+        (p) => p.slug === product.slug
       );
+      let updatedCart;
 
       if (existingProductIndex === -1) {
-        console.log(product);
-        return {
-          cart: [
-            ...state.cart,
-            {
-              slug: product.slug,
-              short: product.short,
-              price: product.price,
-              quantity: product.quantity,
-            },
-          ],
-        };
+        updatedCart = [
+          ...state.cart,
+          {
+            slug: product.slug,
+            short: product.short,
+            price: product.price,
+            quantity: product.quantity,
+          },
+        ];
+      } else {
+        updatedCart = [...state.cart];
+        updatedCart[existingProductIndex].quantity += product.quantity;
       }
-      const updatedCart = [...state.cart];
-      updatedCart[existingProductIndex].quantity += product.quantity;
 
-      return { cart: updatedCart };
+      localStorage.setItem("cart", JSON.stringify(updatedCart)); // Save updatedCart to localStorage
+      return { cart: updatedCart }; // Return the updated cart state
     }),
+
   removeProductFromCart: (productSlug) =>
-    set((state) => ({
-      cart: state.cart.filter((product) => product.slug !== productSlug),
-    })),
-  clearCart: () => set({ cart: [] }),
+    set((state) => {
+      const newCart = state.cart.filter(
+        (product) => product.slug !== productSlug
+      );
+      localStorage.setItem("cart", JSON.stringify(newCart));
+      return { cart: newCart };
+    }),
+  clearCart: () => {
+    localStorage.removeItem("cart"); // Clear from localStorage
+    set({ cart: [] });
+  },
 
   incrementProductQuantity: (productSlug) =>
     set((state) => {
       const existingProductIndex = state.cart.findIndex(
-        (p) => p.slug === productSlug,
+        (p) => p.slug === productSlug
       );
 
       if (existingProductIndex !== -1) {
         const updatedCart = [...state.cart];
         updatedCart[existingProductIndex].quantity += 1;
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
         return { cart: updatedCart };
       }
 
-      return state; // Return unchanged state if the product isn't found
+      return state;
     }),
 
   decrementProductQuantity: (productSlug) =>
     set((state) => {
       const existingProductIndex = state.cart.findIndex(
-        (p) => p.slug === productSlug,
+        (p) => p.slug === productSlug
       );
 
       if (
@@ -66,6 +85,7 @@ const useStore = create((set) => ({
           updatedCart.splice(existingProductIndex, 1);
         }
 
+        localStorage.setItem("cart", JSON.stringify(updatedCart));
         return { cart: updatedCart };
       }
 
