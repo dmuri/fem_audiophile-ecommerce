e87bc16cbd07428b7e2c0de4a1933a06842d5dc8
Daniel Muri <danmur26@gmail.com>
2023-09-24 16:46:20 +0200

    implemented scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,35 @@
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Root from "./routes/root.jsx";
 import Home from "./routes/home.jsx";
 import Category from "./routes/category.jsx";
 import Checkout from "./routes/checkout.jsx";
 import Product from "./routes/product.jsx";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "category/:category",
-        element: <Category />,
-      },
-      {
-        path: "/product/:slug",
-        element: <Product />,
-      },
-    ],
-  },
-  {
-    path: "/checkout",
-    element: <Checkout />,
-  },
-]);
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
-    <RouterProvider router={router}>
-      <Outlet />
-    </RouterProvider>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="category/:category" element={<Category />} />
+          <Route path="product/:slug" element={<Product />} />
+        </Route>
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+    </Router>
   );
 }
 
